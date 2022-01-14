6d2128b88bafb079d5b1683ca883a8f075c62844
Milton Castro <milton.castro@liferay.com>
2022-01-14 10:27:23 -0300

    LPS-145400 Clean up test setup

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-field-type/jest-setup.config.js
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-field-type/jest-setup.config.js
@@ -12,62 +12,11 @@
  * details.
  */
 
-window.AlloyEditor = {
-	...window.AlloyEditor,
-	Selections: [
-		{
-			buttons: ['linkEdit'],
-			name: 'link',
-		},
-		{
-			buttons: [
-				'styles',
-				'bold',
-				'italic',
-				'underline',
-				'link',
-				'twitter',
-			],
-			name: 'text',
-		},
-	],
-};
-
-window.AUI = () => ({
-	...window.AUI,
-	use: (...modules) => {
-		const callback = modules[modules.length - 1];
-
-		callback({
-			LiferayAlloyEditor: () => ({
-				render: () => ({
-					destroy: () => {},
-					getHTML: () => 'test',
-					getNativeEditor: () => ({
-						on: () => true,
-						setData: () => false,
-					}),
-				}),
-			}),
-			one: () => ({
-				innerHTML: () => {},
-			}),
-		});
-	},
-});
-
-window.Liferay.PortletKeys = {
-	DOCUMENT_LIBRARY: 'DOCUMENT_LIBRARY',
-	ITEM_SELECTOR: 'ITEM_SELECTOR',
-};
-
 window.themeDisplay = {
 	...window.themeDisplay,
 	getDefaultLanguageId: () => 'en_US',
 	getLanguageId: () => 'en_US',
-	getLayoutRelativeControlPanelURL: () => 'layoutRelativeControlPanelURL',
-	getLayoutRelativeURL: () => 'getLayoutRelativeURL',
-	getScopeGroupId: () => 'scopeGroupId',
+	getPathThemeImages: () => 'http://localhost:8080',
 	isSignedIn: () => true,
 };
 
@@ -80,42 +29,18 @@ function sub(key, ...params) {
 	return text;
 }
 
-window.util = {
-	...window.util,
-	escape: (data) => data,
-	selectEntity: () => {},
-	sub,
-};
-
 const languageMap = {
-	'days-abbreviation': 'd',
-	'decimal-delimiter': '.',
-	'hours-abbreviation': 'h',
-	'minutes-abbreviation': 'min',
-	'mmm-dd': 'MMM DD',
-	'mmm-dd-hh-mm': 'MMM DD, HH:mm',
-	'mmm-dd-hh-mm-a': 'MMM DD, hh:mm A',
-	'mmm-dd-lt': 'MMM DD, LT',
-	'mmm-dd-yyyy': 'MMM DD, YYYY',
-	'mmm-dd-yyyy-lt': 'MMM DD, YYYY, LT',
-	'thousand-abbreviation': 'K',
 	'x-of-x-characters': '{0}/{1} characters',
 };
 
 window.Liferay = {
-	...(window.Liferay || {}),
-	AUI: {
-		getDateFormat: () => '%m/%d/%Y',
-	},
 	Language: {
-		get: (key) => {
-			if (languageMap[key]) {
-				return languageMap[key];
-			}
-
-			return key;
-		},
+		direction: {en_US: 'rtl'},
+		get: (key) => languageMap[key] ?? key,
 	},
 	ThemeDisplay: window.themeDisplay,
-	Util: window.util,
+	Util: {
+		escape: (data) => data,
+		sub,
+	},
 };
